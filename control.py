6d860a6f5df40d7bd2415729898ed21143aa6528
DH0K3B44Z <156872039+DH0K3B44Z@users.noreply.github.com>
2025-09-30 17:52:30 +0530

    Update control.py

--- control.py
+++ control.py
@@ -3,7 +3,7 @@ import requests
 import random
 import time
 
-API_URL = 'http://127.0.0.1:5000'  # change to server IP/domain
+API_URL = 'fi7.bot-hosting.net:20112'  # change to server IP/domain
 
 USER_AGENTS = [
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64)",
